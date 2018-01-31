4f301be3b90b944c930f02bd4292f138802e4dda
addissu <atamru05@gmail.com>
2018-01-30 21:36:12 -0800

    added new code for the converting of fahrenheit to celsius

--- app/src/main/java/com/example/addissutamru/tempconverter/MainActivity.java
+++ app/src/main/java/com/example/addissutamru/tempconverter/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.addissutamru.tempconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView DegreeTextView;
+    private EditText FahrenheitEditText;
+    private EditText celsiusEditText;
+    private Button ConvertButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    //connecting the xml to java code
+    ConvertButton = (Button) findViewById(R.id.ConvertButton);
+    celsiusEditText = (EditText) findViewById(R.id.celsiusEditText);
+    FahrenheitEditText = (EditText) findViewById(R.id.fahrenheitEditText);
+    DegreeTextView = (TextView) findViewById(R.id.DegreetextView);
+
+
+        //onclick listener to convert the temp to celsius
+        ConvertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // get information
+                double degree = Double.valueOf(FahrenheitEditText.getText().toString());
+                //math converter
+                double cells = (degree  - 32) * 5 / 9;
+                //set celsius after degree has been input by user
+                celsiusEditText.setText(String.valueOf(cells));
+
+
+            }
+
+
+        });
+
     }
+
 }
